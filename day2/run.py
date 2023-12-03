971363c02cdcbc116af48716c8aaa8d145744222
Doug Ramirez <doug@ramirez.fyi>
2023-12-03 12:20:02 -0500

    Initial commit of day 2 part 2

--- day2/run.py
+++ day2/run.py
@@ -1,8 +1,24 @@
-from collections import OrderedDict
-
 MAX_RED_CUBES = 12
 MAX_GREEN_CUBES = 13
 MAX_BLUE_CUBES = 14
+
+
+def cubes_needed_per_game(sets):
+    red_cubes_needed = 0
+    green_cubes_needed = 0
+    blue_cubes_needed = 0
+
+    for set in sets:
+        if set[0] > red_cubes_needed:
+            red_cubes_needed = set[0]
+
+        if set[1] > green_cubes_needed:
+            green_cubes_needed = set[1]
+
+        if set[2] > blue_cubes_needed:
+            blue_cubes_needed = set[2]
+
+    return red_cubes_needed, green_cubes_needed, blue_cubes_needed
 
 
 def is_game_possible(sets) -> bool:
@@ -53,9 +69,16 @@ def parse_game(game: str):
 # Open the calibration document
 games = open("./day2/games.txt", "r")
 game_id_number_sum = 0
+game_power_sum = 0
 for game in games:
     game_id_number, sets = parse_game(game)
     if is_game_possible(sets):
         game_id_number_sum += int(game_id_number)
 
+    red_cubes_needed, green_cubes_needed, blue_cubes_needed = cubes_needed_per_game(
+        sets
+    )
+    game_power_sum += red_cubes_needed * green_cubes_needed * blue_cubes_needed
+
 print(f"The sum of all possible game IDs is: {game_id_number_sum}")
+print(f"The sum of all game powers is: {game_power_sum}")
